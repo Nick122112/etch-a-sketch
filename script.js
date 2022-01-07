ad2ab14377e49eb042c68f33530da69307654713
Nicholas Kallas <kallasnicholas@gmail.com>
2022-01-07 12:00:42 -0600

    created a mouseover event listener that changes the color of the sketchboxes when a user hovers them

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
 const sketchContainer = document.createElement("div");
 sketchContainer.setAttribute(
   "style",
-  "width: 64rem; background-color: white; display: flex; flex-wrap: wrap;"
+  "width: 64rem; background-color: black; display: flex; flex-wrap: wrap;"
 );
 document.body.appendChild(sketchContainer);
 
@@ -12,10 +12,28 @@ document.body.appendChild(sketchContainer);
 const sketchBox = document.createElement("div");
 sketchBox.setAttribute(
   "style",
-  "width: 3.8rem; height: 3.8rem; background-color: black; margin:1px;"
+  "width: 3.8rem; height: 3.8rem; background-color: white; margin:1px;"
 );
+
+// change color function
+function changeColor() {
+  this.setAttribute("style", "color: purple");
+}
 
 // cloning 256 sketch boxes
 for (let i = 0; i < 256; i++) {
   sketchContainer.appendChild(sketchBox.cloneNode());
 }
+
+// assigns the variables sketchboxes to all the divs
+const sketchBoxes = sketchContainer.querySelectorAll("div");
+
+// function to change color of sketchboxes
+function changeColor() {
+  this.style.backgroundColor = "black";
+}
+
+// for each sketchbox add an event listener that changes the color
+sketchBoxes.forEach((sketchBox) => {
+  sketchBox.addEventListener("mouseover", changeColor);
+});
